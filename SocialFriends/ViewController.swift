3e86292f9a3066494aca35a569e5fefbc9e67c25
Jógvan Olsen <jo@jebster.net>
2016-03-05 21:35:00 +0100

    Initial Commit

---
+++ SocialFriends/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialFriends
+//
+//  Created by Jógvan Olsen on 3/5/16.
+//  Copyright © 2016 Jógvan Olsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
